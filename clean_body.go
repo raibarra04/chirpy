2e9eefe2b6899a74744e992bdc01f4b46ae7e633
raibarra04 <ribarra764@gmail.com>
2026-09-08 14:28:54 -0700

    feat: no-ref: added validation for bad words

---
+++ clean_body.go
@@ -0,0 +1,16 @@
+package main
+
+import "strings"
+
+
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
+}
